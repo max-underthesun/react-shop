f839225ba0f3754a3a1af3d2cf19bb8bd547b5f4
max_underthesun <max.underthesun@gmail.com>
2018-11-13 10:44:26 +0400

    initial commit: project start

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/App',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000
+  },
+
+  output: {
+    path: path.join(process.cwd(), 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/assets/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'src'),
+    ]
+  }
+};
