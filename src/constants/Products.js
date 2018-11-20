650d58ed945ebb2f624d80060d40f11ee467c418
max_underthesun <max.underthesun@gmail.com>
2018-11-20 09:34:32 +0400

    move src/ to root, add prop-types

--- src/constants/Products.js
+++ src/constants/Products.js
@@ -3,24 +3,24 @@ export const PRODUCTS = [
     id: '1',
     title: 'Mercedes GLE',
     price: 100000,
-    imageUrl: '/src/static/images/2018_mercedes_benz_gle_class_reactfront.jpg'
+    imageUrl: '/static/images/2018_mercedes_benz_gle_class_reactfront.jpg'
   },
   {
     id: '2',
     title: 'Kia Sorento',
     price: 40000,
-    imageUrl: '/src/static/images/2018_kia_sorento_reactfront.jpg'
+    imageUrl: '/static/images/2018_kia_sorento_reactfront.jpg'
   },
   {
     id: '3',
     title: 'Honda Odyssey',
     price: 38000,
-    imageUrl: '/src/static/images/2018_honda_odyssey_reactfront.jpg'
+    imageUrl: '/static/images/2018_honda_odyssey_reactfront.jpg'
   },
   {
     id: '4',
     title: 'Ford Mustang',
     price: 70000,
-    imageUrl: '/src/static/images/2019_ford_mustang_reactfront.jpg'
+    imageUrl: '/static/images/2019_ford_mustang_reactfront.jpg'
   }
 ];
