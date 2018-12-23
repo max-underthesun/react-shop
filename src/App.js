e41f271b93d3748290342e802989164132afb5e1
max_underthesun <max.underthesun@gmail.com>
2018-12-23 12:34:22 +0400

    separate Cart state and contextCart.Provider functionality with Router functionality

--- src/App.js
+++ src/App.js
@@ -5,47 +5,98 @@ import {
 
 import routes from 'routes';
 import { mainPath, contactsPath } from 'helpers/routes';
-import contextCart from 'context/contextCart';
+// import contextCart from 'context/contextCart';
+import CartContainer from 'containers/CartContainer';
 
 const RouteWithSubroutes = (route, key) => (
   React.createElement(Route, { ...route, key })
 );
 
-class AppPageContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cart: []
-    };
-    this.addProduct = this.addProduct.bind(this);
-  }
+// class AppPageContainer extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       cart: []
+//     };
+//     this.addProduct = this.addProduct.bind(this);
+//   }
 
-  addProduct(product) {
-    this.setState((state) => ({ cart: [...state.cart, product] }));
-  }
+//   addProduct(product) {
+//     this.setState((state) => ({ cart: [...state.cart, product] }));
+//   }
 
-  render() {
-    const { cart } = this.state;
-    return (
-      <contextCart.Provider value={{ cart, addProduct: this.addProduct }}>
-        <Router>
-          <div>
-            <ul>
-              <li><NavLink exact to={mainPath()}>Main</NavLink></li>
-              <li><NavLink to={contactsPath()}>Contacts</NavLink></li>
-            </ul>
-            <Switch>
-              {routes.map((route, key) => RouteWithSubroutes(route, key))}
-            </Switch>
-          </div>
-        </Router>
-      </contextCart.Provider>
-    );
-  }
-}
+//   render() {
+//     const { cart } = this.state;
+//     return (
+//       <contextCart.Provider value={{ cart, addProduct: this.addProduct }}>
+//         {this.props.children}
+//         {/* <Router>
+//           <div>
+//             <ul>
+//               <li><NavLink exact to={mainPath()}>Main</NavLink></li>
+//               <li><NavLink to={contactsPath()}>Contacts</NavLink></li>
+//             </ul>
+//             <Switch>
+//               {routes.map((route, key) => RouteWithSubroutes(route, key))}
+//             </Switch>
+//           </div>
+//         </Router> */}
+//       </contextCart.Provider>
+//     );
+//   }
+// }
 
 const App = () => (
-  <AppPageContainer />
+  <CartContainer>
+    <Router>
+      <div>
+        <ul>
+          <li><NavLink exact to={mainPath()}>Main</NavLink></li>
+          <li><NavLink to={contactsPath()}>Contacts</NavLink></li>
+        </ul>
+        <Switch>
+          {routes.map((route, key) => RouteWithSubroutes(route, key))}
+        </Switch>
+      </div>
+    </Router>
+  </CartContainer>
 );
+
+// class AppPageContainer extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       cart: []
+//     };
+//     this.addProduct = this.addProduct.bind(this);
+//   }
+
+//   addProduct(product) {
+//     this.setState((state) => ({ cart: [...state.cart, product] }));
+//   }
+
+//   render() {
+//     const { cart } = this.state;
+//     return (
+//       <contextCart.Provider value={{ cart, addProduct: this.addProduct }}>
+//         <Router>
+//           <div>
+//             <ul>
+//               <li><NavLink exact to={mainPath()}>Main</NavLink></li>
+//               <li><NavLink to={contactsPath()}>Contacts</NavLink></li>
+//             </ul>
+//             <Switch>
+//               {routes.map((route, key) => RouteWithSubroutes(route, key))}
+//             </Switch>
+//           </div>
+//         </Router>
+//       </contextCart.Provider>
+//     );
+//   }
+// }
+
+// const App = () => (
+//   <AppPageContainer />
+// );
 
 export default App;
---
+++ src/containers/CartContainer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import contextCart from 'context/contextCart';
+
+class CartContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: []
+    };
+    this.addProduct = this.addProduct.bind(this);
+  }
+
+  addProduct(product) {
+    this.setState((state) => ({ cart: [...state.cart, product] }));
+  }
+
+  render() {
+    const { cart } = this.state;
+    return (
+      <contextCart.Provider value={{ cart, addProduct: this.addProduct }}>
+        {this.props.children}
+      </contextCart.Provider>
+    );
+  }
+}
+
+
+CartContainer.propTypes = {
+  children: PropTypes.node
+};
+
+export default CartContainer;
